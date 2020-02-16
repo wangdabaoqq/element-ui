724ca9d9b25f9340d0badf4e70a31f8f3575e3bf
wangdabaoqq <614692339@qq.com>
2020-02-16 16:47:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,159 @@
+<template>
+  <div id="app" class="flex-col">
+   
+    <!-- <RadioGroup size="small" @change="changes" v-model="radio">
+      <Radio border :label="3">111</Radio>
+      <Radio border  :label="4">22</Radio>
+    </RadioGroup> -->
+    <list />
+  </div>
+</template>
+
+<script>
+//  import Row from 'element-ui/packages/row'
+//  import Cols from 'element-ui/packages/col'
+//  import Links from 'element-ui/packages/link'
+//  import Radio from 'element-ui/packages/radio'
+//  import RadioGroup from 'element-ui/packages/radio-group'
+
+//  import 'element-ui/lib/theme-chalk/index.css';
+
+
+export default {
+  name: "app",
+  components: {
+    list: () => import('./components/list')
+    // Row,
+    // Cols,
+    // Icon
+    // Links
+    // Radio,
+    // RadioGroup
+  },
+  data() {
+    return {
+    }
+  },
+};
+</script>
+
+<style>
+html,body{height: 100%;}
+body {
+  margin: 0;
+}
+/* 自定义按钮样式 */
+.tree_extra_btn{
+  display: inline-block;
+  padding: 0 3px;
+  color: red;
+}
+
+/* flex栅格 */
+.flex-col {
+  overflow: hidden;
+  display: -ms-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-direction: column;
+  -ms-flex-direction: column;
+}
+
+.flex-row {
+  overflow: hidden;
+  display: -ms-box;
+  display: -ms-flexbox;
+  display: flex;
+}
+
+.flex-1 {
+  -ms-flex: 1;
+  flex: 1;
+  min-width: 0;
+}
+
+.flex-2 {
+  -ms-flex: 2;
+  flex: 2;
+  min-width: 0;
+}
+
+.flex-3 {
+  -ms-flex: 3;
+  flex: 3;
+  min-width: 0;
+}
+
+.flex-4 {
+  -ms-flex: 4;
+  flex: 4;
+  min-width: 0;
+}
+
+.align-stretch {
+  -ms-align-items: stretch;
+  -ms-flex-align: stretch;
+  align-items: stretch;
+}
+
+.align-center {
+  -ms-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+.justify-center {
+  -ms-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+}
+
+#app {
+  height: 100%;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.wrap{
+  overflow: hidden;
+}
+.c {
+  width: 600px;
+  height: 100%;
+  overflow: auto;
+  margin: auto;
+}
+.T {
+  font-size: 34px;
+  margin: 0 0 30px;
+  height: 170px;
+  line-height: 260px;
+  overflow: hidden;
+  background:url(./assets/logo.png) center no-repeat;
+}
+.toolbar {
+  margin: 20px auto;
+}
+.toolbar .btn {
+  padding: 0.5em 1em;
+  outline: none;
+  border-radius: 4px;
+}
+
+.foot {
+  margin-top: 30px;
+  background: #333;
+  padding: 24px;
+  overflow: hidden;
+  color: #999;
+  font-size: 14px;
+  text-align: center;
+}
+
+.foot a {
+  color: #fff;
+  margin: 0 0.5em;
+}
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <!-- <RadioGroup size="small" v-model="radio" @change="changes">
+      <Radio border :label="3">111</Radio>
+      <Radio border  :label="4">22</Radio>
+    </RadioGroup> -->
+    <!-- <Row :gutter="20" type="flex" justify="end">
+      <Cols :span="6">2</Cols>
+      <Cols :span="6">2</Cols>
+      <Cols :span="6">2</Cols>
+      <Cols :span="6">2</Cols>
+    </Row> -->
+    <!-- <checkboxGroup v-model="radio"  :min="1" :max="2"> -->
+    <!-- <checkbox label="选中且禁用" :indeterminate="true" checked true-label="222" size="small" border v-model="radios" @change="changes">备选项e1</checkbox>
+    <checkbox label="123" disabled>备选项2</checkbox>
+    <checkbox label="2333">备选项3</checkbox>
+    <checkbox label="11">备选项4</checkbox> -->
+    <!-- </checkboxGroup> -->
+    <!-- <checkboxBut>111</checkboxBut> -->
+    <InputNumber v-model="text" :min="1" :max="10"  />
+    <!-- <Input v-model="text" size="small" suffix-icon="el-icon-date"  :autosize="{ minRows: 1, maxRows: 4}" clearable type="textarea"  /> -->
+  </div>
+</template>
+
+<script>
+// import Row from 'element-ui/packages/row'
+// import Cols from 'element-ui/packages/col'
+//  import Radio from 'element-ui/packages/radio'
+//  import RadioGroup from 'element-ui/packages/radio-group'
+//  import checkbox from 'element-ui/packages/checkbox'
+//  import checkboxBut from 'element-ui/packages/checkbox-button'
+ import InputNumber from 'element-ui/packages/input-number'
+
+//  import checkboxGroup from 'element-ui/packages/checkbox-group'
+
+ import 'element-ui/lib/theme-chalk/index.css';
+
+export default {
+  data () {
+    return {
+      radio: ['选中且禁用','123'],
+      radios: false,
+      text: 1
+    }
+  },
+  methods: {
+     changes () {
+      console.log('radios')
+    }
+  },
+  components: {
+    // tree: resolve => require(["./components/ztree.vue"], resolve),
+    // setList: () => import ('./setList'),
+    // Row,
+    // Cols
+    // Icon
+    // Links
+    // Radio,
+    // RadioGroup
+    // checkbox,
+    // checkboxBut
+    // Input
+    InputNumber
+    // checkboxGroup
+  },
+}
+</script>
+
+<style>
+
+</style>
